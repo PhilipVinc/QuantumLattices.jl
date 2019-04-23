279eae0c1f8643887985cae19d2dfb7174458051
Filippo Vicentini <filippovicentini@gmail.com>
2019-04-23 14:33:12 +0200

    Shorthands for creating observables

--- src/Utils/Observable.jl
+++ src/Utils/Observable.jl
@@ -3,12 +3,32 @@ export LocalObservable
 """
     LocalObservable(lindbladian, op)
 """
-function LocalObservable(lind::GraphLindbladian, op, norm=1.0)
+function MeanObservable(lind::Union{GraphLindbladian, GraphOperator},
+                         op, norm=1.0)
     Hilb = basis(lind)
     !is_homogeneous(Hilb) && error("The basis should be homogeneous.")
     if isa(op, Function)
         op = op(first(Hilb.bases))
     end
 
-    GraphOperator(graph(lind), Hilb, op/norm)
+    GraphOperator(graph(lind), Hilb, op*norm)
+end
+
+function LocalObservable(lind::Union{GraphLindbladian, GraphOperator},
+                              op, sites)
+    Hilb = basis(lind)
+    gop = GraphOperator(graph(lind), Hilb)
+
+    if isa(op, Function) && isa(sites, Int)
+        op = op(Hilb.bases[sites])
+    elseif isa(op, Function)
+    end
+
+    if isa(sites, Int)
+        if isa(op, Function)
+            op = op(Hilb.bases[sites])
+        end
+        add_local_operator!(gop, op, sites)
+    end
+    gop
 end
