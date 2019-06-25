0b28e0b587208a73868fb27307d245e8a644bd8d
Filippo Vicentini <filippovicentini@gmail.com>
2019-06-25 14:34:12 +0200

    Fix export

--- src/QuantumLattices.jl
+++ src/QuantumLattices.jl
@@ -26,7 +26,7 @@ abstract type AbstractGraphOperator end
 abstract type AbstractGraphSuperOperator end
 
 export graph, basis, data, is_homogeneous
-export add_local_operator!, add_hop_operator!, SparseOperator
+export add_local_operator!, add_hop_operator!, SparseOperator, DenseOperator
 export GraphOperator, GraphLindbladian, hamiltonian, jump_operators, liouvillian
 export add_homogeneous_dissipation, add_dissipator
 
